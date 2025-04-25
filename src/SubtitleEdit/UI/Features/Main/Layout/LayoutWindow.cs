3d8f6ee73c6133b6aa0fe1ef40bc88f03da19d2a
niksedk <nikse.dk@gmail.com>
2025-04-25 06:54:09 +0200

    Work on layout

--- src/SubtitleEdit/UI/Features/Main/Layout/LayoutWindow.cs
+++ src/SubtitleEdit/UI/Features/Main/Layout/LayoutWindow.cs
@@ -1,5 +1,7 @@
 using System;
+using System.Collections.Generic;
 using System.Globalization;
+using System.Threading.Tasks;
 using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Data.Converters;
@@ -13,6 +15,7 @@ namespace Nikse.SubtitleEdit.Features.Main.Layout;
 public class LayoutWindow : Window
 {
     private LayoutModel _vm;
+    private List<Border> _borders = new List<Border>();
 
     public LayoutWindow(LayoutModel viewModel)
     {
@@ -39,11 +42,32 @@ public class LayoutWindow : Window
                 Width = 200,
                 Height = 139
             };
+            
+            var text = new TextBlock
+            {
+                Text = i.ToString(CultureInfo.InvariantCulture),
+                HorizontalAlignment = HorizontalAlignment.Center,
+                VerticalAlignment = VerticalAlignment.Center,
+                Foreground = Brushes.White, 
+                FontSize = 34,
+                FontWeight = FontWeight.Bold,
+                Opacity = 0.7,
+            };
+
+            // Layer the image and text
+            var grid = new Grid
+            {
+                Width = 200,
+                Height = 139
+            };
+
+            grid.Children.Add(image);
+            grid.Children.Add(text);
 
             var border = new Border
             {
                 Margin = new Thickness(10),
-                Child = image,
+                Child = grid,
                 RenderTransformOrigin = new RelativePoint(0.5, 0.5, RelativeUnit.Relative),
                 RenderTransform = new ScaleTransform(1, 1)
             };
@@ -68,40 +92,40 @@ public class LayoutWindow : Window
             };
 
             wrapPanel.Children.Add(border);
+            _borders.Add(border);
         }
 
         Content = new ScrollViewer { Content = wrapPanel };
     }
 
-    protected override void OnKeyDown(KeyEventArgs e)
+    protected override async void OnKeyDown(KeyEventArgs e)
     {
         base.OnKeyDown(e);
         if (e.Key == Key.Escape)
         {
             Close();
         }
-    }
-
-
-    public class HoverToBrushConverter : IValueConverter
-    {
-        public object Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+        
+        var layoutLookup = new Dictionary<Key, int>
         {
-            return (bool)value! ? Brushes.LightGray : Brushes.Transparent;
-        }
-
-        public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
-            => throw new NotImplementedException();
-    }
-
-    public class HoverToScaleTransformConverter : IValueConverter
-    {
-        public object Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+            { Key.D1, 0 },
+            { Key.D2, 1 },
+            { Key.D3, 2 },
+            { Key.D4, 3 },
+            { Key.D5, 4 },
+            { Key.D6, 5 },
+            { Key.D7, 6 },
+            { Key.D8, 7 },
+            { Key.D9, 8 },
+        };
+        if (layoutLookup.TryGetValue(e.Key, out var layoutNumber))        
         {
-            return (bool)value! ? new ScaleTransform(1.1, 1.1) : new ScaleTransform(1.0, 1.0);
+            _borders[layoutNumber].RenderTransform = new ScaleTransform(1.1, 1.1);
+            _borders[layoutNumber].Background = Brushes.DarkSeaGreen;
+            await Task.Delay(500);
+            
+            _vm.SelectedLayout = layoutNumber;
+            Close();
         }
-
-        public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
-            => throw new NotImplementedException();
     }
 }
