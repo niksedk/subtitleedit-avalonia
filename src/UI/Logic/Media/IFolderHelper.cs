28e00d26861493bd2fb706d982252712f2840bf6
niksedk <nikse.dk@gmail.com>
2025-08-04 18:08:26 +0200

    testing yml

--- src/UI/Logic/Media/IFolderHelper.cs
+++ src/UI/Logic/Media/IFolderHelper.cs
@@ -1,6 +1,8 @@
-﻿using Avalonia.Controls;
+﻿using System.Diagnostics;
+using Avalonia.Controls;
 using Avalonia.Platform.Storage;
 using System.IO;
+using System.Runtime.InteropServices;
 using System.Threading.Tasks;
 
 namespace Nikse.SubtitleEdit.Logic.Media;
@@ -9,6 +11,7 @@ public interface IFolderHelper
 {
     Task<string> PickFolderAsync(Window window, string title);
     Task OpenFolder(Window window, string folder);
+    Task OpenFolderWithFileSelected(Window window, string selectedFile);
 }
 
 public class FolderHelper : IFolderHelper
@@ -34,8 +37,47 @@ public class FolderHelper : IFolderHelper
 
     public async Task OpenFolder(Window window, string folder)
     {
-        var path = Path.GetDirectoryName(folder)!;
-        var dirInfo = new DirectoryInfo(path);
-        await window!.Launcher.LaunchDirectoryInfoAsync(dirInfo);
+        if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+        {
+            var startInfo = new ProcessStartInfo
+            {
+                FileName = "open",
+                Arguments = $"\"{folder}\"",
+                UseShellExecute = false,
+                CreateNoWindow = true
+            };
+
+            using var process = Process.Start(startInfo);
+            return;
+        }
+
+        var dirInfo = new DirectoryInfo(folder);
+        await window.Launcher.LaunchDirectoryInfoAsync(dirInfo);
+    }
+    
+    public async Task OpenFolderWithFileSelected(Window window, string selectedFile)
+    {
+        if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+        {
+            var startInfo = new ProcessStartInfo
+            {
+                FileName = "open",
+                Arguments = $"-R \"{selectedFile}\"", // -R flag reveals the file in Finder
+                UseShellExecute = false,
+                CreateNoWindow = true
+            };
+
+            using var process = Process.Start(startInfo);
+            return;
+        }
+
+        var folder = Path.GetDirectoryName(selectedFile);
+        if (folder == null)
+        {
+            return;
+        }
+
+        var dirInfo = new DirectoryInfo(folder);
+        await window.Launcher.LaunchDirectoryInfoAsync(dirInfo);
     }
 }
