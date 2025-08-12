1fd27ac206c6afb006fee85eb7e16e4415d5f8ba
Nikolaj Olsson <nikse.dk@gmail.com>
2025-08-12 13:42:51 +0200

    Work on OCR

--- src/UI/Logic/Download/PaddleOcrDownloadService.cs
+++ src/UI/Logic/Download/PaddleOcrDownloadService.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.IO;
 using System.Net.Http;
+using System.Runtime.InteropServices;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -12,6 +13,7 @@ public class PaddleOcrDownloadService : IPaddleOcrDownloadService
     public const string DownloadModelsUrl = "https://github.com/timminator/PaddleOCR-Standalone/releases/download/v1.3.0/PaddleOCR.PP-OCRv5.support.files.VideOCR.7z";
     public const string DownloadWindowsEngineCpuUrl = "https://github.com/timminator/PaddleOCR-Standalone/releases/download/v1.3.0/PaddleOCR-CPU-v1.3.0.7z";
     public const string DownloadWindowsEngineGpuUrl = "https://github.com/timminator/PaddleOCR-Standalone/releases/download/v1.3.0/PaddleOCR-GPU-v1.3.0-CUDA-11.8.7z";
+    public const string DownloadLinuxEngineCpuUrl = "https://github.com/timminator/PaddleOCR-Standalone/releases/download/v1.3.0/PaddleOCR-CPU-v1.3.0-Linux.tar.xz";
 
     public PaddleOcrDownloadService(HttpClient httpClient)
     {
@@ -27,6 +29,12 @@ public class PaddleOcrDownloadService : IPaddleOcrDownloadService
     public async Task DownloadEngineCpu(string destinationFileName, IProgress<float>? progress, CancellationToken cancellationToken)
     {
         var url = DownloadWindowsEngineCpuUrl;
+
+        if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+        {
+            url = DownloadLinuxEngineCpuUrl;
+        }
+
         await DownloadHelper.DownloadFileAsync(_httpClient, url, destinationFileName, progress, cancellationToken);
     }
 
