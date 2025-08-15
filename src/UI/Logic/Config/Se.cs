b9583dd64cf4250f70eec5921722b51371dfa054
Nikolaj Olsson <nikse.dk@gmail.com>
2025-08-15 15:47:43 +0200

    Update version

--- src/UI/Logic/Config/Se.cs
+++ src/UI/Logic/Config/Se.cs
@@ -154,7 +154,7 @@ public class Se
     public static string FfmpegFolder => Path.Combine(DataFolder, "ffmpeg");
     public static string WhisperFolder => Path.Combine(DataFolder, "Whisper");
 
-    public static string Version { get; set; } = "v5.0.0-preview18";
+    public static string Version { get; set; } = "v5.0.0-preview19";
 
     public Se()
     {
