25fa611061845e12d85bf102d02fc02ca403e609
niksedk <nikse.dk@gmail.com>
2025-10-24 08:21:36 +0200

    Work on "Add video offset"

---
+++ src/UI/Features/Shared/SetVideoOffset/SetVideoOffsetViewModel.cs
@@ -0,0 +1,50 @@
+using System;
+using Avalonia.Controls;
+using Avalonia.Input;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace Nikse.SubtitleEdit.Features.Shared.SetVideoOffset;
+
+public partial class SetVideoOffsetViewModel : ObservableObject
+{
+    [ObservableProperty] private TimeSpan? _timeOffset;
+    
+    public Window? Window { get; set; }
+
+    public bool OkPressed { get; private set; }
+
+    public SetVideoOffsetViewModel()
+    {
+    }
+
+    public void Initialize()
+    {
+    }
+
+    [RelayCommand]                   
+    private void Ok() 
+    {
+        OkPressed = true;
+        Window?.Close();
+    }
+    
+    [RelayCommand]                   
+    private void Cancel() 
+    {
+        Window?.Close();
+    }
+
+    public void OnKeyDown(KeyEventArgs e)
+    {
+        if (e.Key == Key.Escape)
+        {
+            e.Handled = true;
+            Cancel();
+        }
+        else if (e.Key == Key.Enter)
+        {
+            Ok();
+        }
+    }
+}
