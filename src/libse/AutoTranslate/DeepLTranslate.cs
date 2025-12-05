6389a205c0fa016c9a82398822216352a2cfccc8
Nikolaj Olsson <nikse.dk@gmail.com>
2025-12-05 07:58:02 +0100

    Include libse

---
+++ src/libse/AutoTranslate/DeepLTranslate.cs
@@ -0,0 +1,233 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using Nikse.SubtitleEdit.Core.Common;
+using Nikse.SubtitleEdit.Core.Translate;
+
+namespace Nikse.SubtitleEdit.Core.AutoTranslate
+{
+    /// <summary>
+    /// DeepL Pro V2 translator - see https://www.deepl.com/api.html
+    /// </summary>
+    public class DeepLTranslate : IAutoTranslator, IDisposable
+    {
+        private string _apiKey;
+        private string _apiUrl;
+        private string _formality;
+        private HttpClient _httpClient;
+
+        public static string StaticName { get; set; } = "DeepL V2 translate";
+        public override string ToString() => StaticName;
+        public string Name => StaticName;
+        public string Url => "https://www.deepl.com";
+        public string Error { get; set; }
+        public int MaxCharacters => 1500;
+
+        public void Initialize()
+        {
+            _apiKey = Configuration.Settings.Tools.AutoTranslateDeepLApiKey;
+            _apiUrl = Configuration.Settings.Tools.AutoTranslateDeepLUrl;
+            _formality = Configuration.Settings.Tools.AutoTranslateDeepLFormality;
+
+            if (string.IsNullOrEmpty(_apiKey) || string.IsNullOrEmpty(_apiUrl))
+            {
+                return;
+            }
+
+            _httpClient = HttpClientFactoryWithProxy.CreateHttpClientWithProxy();
+            _httpClient.BaseAddress = new Uri(_apiUrl.Trim().TrimEnd('/'));
+            _httpClient.DefaultRequestHeaders.TryAddWithoutValidation("Authorization", "DeepL-Auth-Key " + _apiKey.Trim());
+            _formality = string.IsNullOrWhiteSpace(_formality) ? "default" : _formality.Trim();
+        }
+
+        public List<TranslationPair> GetSupportedSourceLanguages()
+        {
+            return new List<TranslationPair>
+            {
+                MakeTranslationPair("Arabic", "ar"),
+                MakeTranslationPair("Bulgarian", "bg"),
+                MakeTranslationPair("Chinese", "zh"),
+                MakeTranslationPair("Czech", "cs"),
+                MakeTranslationPair("Danish", "da"),
+                MakeTranslationPair("Dutch", "nl", true),
+                MakeTranslationPair("English", "en", true),
+                MakeTranslationPair("Estonian", "et"),
+                MakeTranslationPair("Finnish", "fi"),
+                MakeTranslationPair("French", "fr", true),
+                MakeTranslationPair("German", "de", true),
+                MakeTranslationPair("Greek", "el"),
+                MakeTranslationPair("Hebrew", "he"),
+                MakeTranslationPair("Hungarian", "hu"),
+                MakeTranslationPair("Indonesian", "id"),
+                MakeTranslationPair("Italian", "it", true),
+                MakeTranslationPair("Japanese", "ja", true),
+                MakeTranslationPair("Korean", "ko"),
+                MakeTranslationPair("Latvian", "lv"),
+                MakeTranslationPair("Lithuanian", "lt"),
+                MakeTranslationPair("Norwegian (Bokmål)", "nb"),
+                MakeTranslationPair("Polish", "pl", true),
+                MakeTranslationPair("Portuguese", "pt", true),
+                MakeTranslationPair("Romanian", "ro"),
+                MakeTranslationPair("Russian", "ru", true),
+                MakeTranslationPair("Slovak", "sk"),
+                MakeTranslationPair("Slovenian", "sl"),
+                MakeTranslationPair("Spanish", "es", true),
+                MakeTranslationPair("Swedish", "sv"),
+                MakeTranslationPair("Turkish", "tr"),
+                MakeTranslationPair("Ukrainian", "uk"),
+            };
+        }
+
+        public List<TranslationPair> GetSupportedTargetLanguages()
+        {
+            return new List<TranslationPair>
+            {
+                MakeTranslationPair("Arabic", "ar"),
+                MakeTranslationPair("Bulgarian", "bg"),
+                MakeTranslationPair("Chinese (simplified)", "zh-hans"),
+                MakeTranslationPair("Chinese (traditional)", "zh-hant"),
+                MakeTranslationPair("Czech", "cs"),
+                MakeTranslationPair("Danish", "da"),
+                MakeTranslationPair("Dutch", "nl", true),
+                MakeTranslationPair("English (British)", "en-gb", true),
+                MakeTranslationPair("English (American)", "en-us", true),
+                MakeTranslationPair("Estonian", "et"),
+                MakeTranslationPair("Finnish", "fi"),
+                MakeTranslationPair("French", "fr", true),
+                MakeTranslationPair("German", "de", true),
+                MakeTranslationPair("Greek", "el"),
+                MakeTranslationPair("Hebrew", "he"),
+                MakeTranslationPair("Hungarian", "hu"),
+                MakeTranslationPair("Indonesian", "id"),
+                MakeTranslationPair("Italian", "it", true),
+                MakeTranslationPair("Japanese", "ja", true),
+                MakeTranslationPair("Korean", "ko"),
+                MakeTranslationPair("Latvian", "lv"),
+                MakeTranslationPair("Lithuanian", "lt"),
+                MakeTranslationPair("Norwegian (Bokmål)", "nb"),
+                MakeTranslationPair("Polish", "pl", true),
+                MakeTranslationPair("Portuguese", "pt-pt", true),
+                MakeTranslationPair("Portuguese (Brazil)", "pt-br", true),
+                MakeTranslationPair("Romanian", "ro"),
+                MakeTranslationPair("Russian", "ru", true),
+                MakeTranslationPair("Slovak", "sk"),
+                MakeTranslationPair("Slovenian", "sl"),
+                MakeTranslationPair("Spanish", "es", true),
+                MakeTranslationPair("Swedish", "sv"),
+                MakeTranslationPair("Turkish", "tr"),
+                MakeTranslationPair("Ukrainian", "uk"),
+            };
+        }
+
+        private static TranslationPair MakeTranslationPair(string name, string code)
+        {
+            return new TranslationPair(name, code, code);
+        }
+
+        private static TranslationPair MakeTranslationPair(string name, string code, bool hasFormality)
+        {
+            return new TranslationPair(name, code, hasFormality);
+        }
+
+        public async Task<string> Translate(string text, string sourceLanguageCode, string targetLanguageCode, CancellationToken cancellationToken)
+        {
+            int[] retryDelays = { 555, 3007, 7013 };
+            HttpResponseMessage result = null;
+            string resultContent = null;
+            for (var attempt = 0; attempt <= retryDelays.Length; attempt++)
+            {
+                var postContent = MakeContent(text, sourceLanguageCode, targetLanguageCode);
+                result = await _httpClient.PostAsync("/v2/translate", postContent, cancellationToken);
+                resultContent = await result.Content.ReadAsStringAsync();
+
+                if (!ShouldRetry(result, resultContent) || attempt == retryDelays.Length)
+                {
+                    break;
+                }
+
+                await Task.Delay(retryDelays[attempt], cancellationToken);
+            }
+
+            if (!result.IsSuccessStatusCode)
+            {
+                SeLogger.Error("DeepLTranslate error: " + resultContent);
+            }
+
+            if (result.StatusCode == HttpStatusCode.Forbidden)
+            {
+                Error = resultContent;
+                throw new Exception("Forbidden! " + Environment.NewLine + Environment.NewLine + resultContent);
+            }
+
+            try
+            {
+                var resultList = new List<string>();
+                var parser = new JsonParser();
+                var x = (Dictionary<string, object>)parser.Parse(resultContent);
+                foreach (var k in x.Keys)
+                {
+                    if (x[k] is List<object> mainList)
+                    {
+                        foreach (var mainListItem in mainList)
+                        {
+                            if (mainListItem is Dictionary<string, object> innerDic)
+                            {
+                                foreach (var transItem in innerDic.Keys)
+                                {
+                                    if (transItem == "text")
+                                    {
+                                        var s = innerDic[transItem].ToString();
+                                        resultList.Add(s);
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+
+                return string.Join(Environment.NewLine, resultList);
+            }
+            catch (Exception ex)
+            {
+                SeLogger.Error(ex, "DeepLTranslate.Translate: " + ex.Message + Environment.NewLine + resultContent);
+                throw;
+            }
+        }
+
+        public static bool ShouldRetry(HttpResponseMessage result, string resultContent)
+        {
+            const int httpStatusCodeTooManyRequests = 429;
+
+            return result.StatusCode == HttpStatusCode.ServiceUnavailable ||
+                   (int)result.StatusCode == httpStatusCodeTooManyRequests ||
+                   (result != null && resultContent.Contains("<head><title>429 Too Many Requests</title></head>", StringComparison.Ordinal));
+        }
+
+        private FormUrlEncodedContent MakeContent(string text, string sourceLanguageCode, string targetLanguageCode)
+        {
+            var array = new List<KeyValuePair<string, string>>()
+            {
+                new KeyValuePair<string, string>("text", text),
+                new KeyValuePair<string, string>("target_lang", targetLanguageCode),
+                new KeyValuePair<string, string>("source_lang", sourceLanguageCode),
+            };
+
+            var targetLanguages = GetSupportedTargetLanguages();
+            var hasFormality = targetLanguages.Find(x => x.Code.Equals(targetLanguageCode, StringComparison.OrdinalIgnoreCase))?.HasFormality ?? false;
+            if (hasFormality && !string.IsNullOrEmpty(_formality))
+            {
+                array.Add(new KeyValuePair<string, string>("formality", _formality));
+            }
+
+            return new FormUrlEncodedContent(array);
+        }
+
+        public void Dispose()
+        {
+            _httpClient?.Dispose();
+        }
+    }
+}
---
+++ src/libse/ContainerFormats/XSub.cs
@@ -0,0 +1,134 @@
+﻿using Nikse.SubtitleEdit.Core.Common;
+using SkiaSharp;
+using System;
+using System.Collections.Generic;
+
+namespace Nikse.SubtitleEdit.Core.ContainerFormats
+{
+    public class XSub
+    {
+        public TimeCode Start { get; set; }
+        public TimeCode End { get; set; }
+        public int Width { get; }
+        public int Height { get; }
+
+        private readonly byte[] _colorBuffer;
+        private readonly byte[] _rleBuffer;
+
+        public XSub(string timeCode, int width, int height, byte[] colors, byte[] rle)
+        {
+            Start = DecodeTimeCode(timeCode.Substring(0, 13));
+            End = DecodeTimeCode(timeCode.Substring(13, 12));
+            Width = width;
+            Height = height;
+            _colorBuffer = colors;
+            _rleBuffer = rle;
+        }
+
+        private static TimeCode DecodeTimeCode(string timeCode)
+        {
+            var parts = timeCode.Split(new[] { ':', ';', '.', ',', '-' }, StringSplitOptions.RemoveEmptyEntries);
+            return new TimeCode(int.Parse(parts[0]), int.Parse(parts[1]), int.Parse(parts[2]), int.Parse(parts[3]));
+        }
+
+        private static void GenerateBitmap(FastBitmap bmp, byte[] buf, List<SKColor> fourColors)
+        {
+            int w = bmp.Width;
+            int h = bmp.Height;
+            int nibbleOffset = 0;
+            var nibbleEnd = buf.Length * 2;
+            var x = 0;
+            var y = 0;
+            for (; ; )
+            {
+                if (nibbleOffset >= nibbleEnd)
+                {
+                    return;
+                }
+
+                var v = GetNibble(buf, nibbleOffset++);
+                if (v < 0x4)
+                {
+                    v = (v << 4) | GetNibble(buf, nibbleOffset++);
+                    if (v < 0x10)
+                    {
+                        v = (v << 4) | GetNibble(buf, nibbleOffset++);
+                        if (v < 0x040)
+                        {
+                            v = (v << 4) | GetNibble(buf, nibbleOffset++);
+                            if (v < 4)
+                            {
+                                v |= (w - x) << 2;
+                            }
+                        }
+                    }
+                }
+
+                var len = v >> 2;
+                if (len > w - x)
+                {
+                    len = w - x;
+                }
+
+                var color = v & 0x03;
+                if (color > 0)
+                {
+                    var c = fourColors[color];
+                    bmp.SetPixel(x, y, c, len);
+                }
+
+                x += len;
+                if (x >= w)
+                {
+                    y++;
+                    if (y >= h)
+                    {
+                        break;
+                    }
+
+                    x = 0;
+                    nibbleOffset += (nibbleOffset & 1);
+                }
+            }
+        }
+
+        private static int GetNibble(byte[] buf, int nibbleOffset)
+        {
+            return (buf[nibbleOffset >> 1] >> ((1 - (nibbleOffset & 1)) << 2)) & 0xf;
+        }
+
+        public SKBitmap GetImage(SKColor background, SKColor pattern, SKColor emphasis1, SKColor emphasis2)
+        {
+            var fourColors = new List<SKColor> { background, pattern, emphasis1, emphasis2 };
+            var bmp = new SKBitmap(Width, Height);
+
+            // If background isn't transparent, fill the bitmap with it
+            if (fourColors[0].Alpha != 0)
+            {
+                using (var canvas = new SKCanvas(bmp))
+                {
+                    using (var paint = new SKPaint { Color = fourColors[0] })
+                    {
+                        canvas.DrawRect(0, 0, bmp.Width, bmp.Height, paint);
+                    }
+                }
+            }
+
+            var fastBmp = new FastBitmap(bmp);
+            fastBmp.LockImage();
+            GenerateBitmap(fastBmp, _rleBuffer, fourColors);
+            fastBmp.UnlockImage();
+            return bmp;
+        }
+
+        private SKColor GetColor(int start)
+        {
+            return new SKColor(_colorBuffer[start], _colorBuffer[start + 1], _colorBuffer[start + 2]);
+        }
+
+        public SKBitmap GetImage()
+        {
+            return GetImage(SKColors.Transparent, GetColor(3), GetColor(6), GetColor(9));
+        }
+    }
+}
