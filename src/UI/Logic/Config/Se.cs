9c0efcb671e256d330555c0cb408559520118692
Nikolaj Olsson <nikse.dk@gmail.com>
2025-12-11 11:46:42 +0100

    Update version to v5.0.0-preview73

--- src/UI/Logic/Config/Se.cs
+++ src/UI/Logic/Config/Se.cs
@@ -177,7 +177,7 @@ public class Se
     public static string SpectrogramsFolder => Path.Combine(DataFolder, "Spectrograms");
     public static string ShotChangesFolder => Path.Combine(DataFolder, "ShotChanges");
 
-    public static string Version { get; set; } = "v5.0.0-preview72";
+    public static string Version { get; set; } = "v5.0.0-preview73";
 
     public Se()
     {
