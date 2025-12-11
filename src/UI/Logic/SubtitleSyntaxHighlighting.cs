7aa9ad14e7733e4abf1c792a261272fce68d2a0c
Nikolaj Olsson <nikse.dk@gmail.com>
2025-12-11 18:49:48 +0100

    work on coloring tags

--- src/UI/Logic/SubtitleSyntaxHighlighting.cs
+++ src/UI/Logic/SubtitleSyntaxHighlighting.cs
@@ -1,144 +1,180 @@
 using Avalonia.Media;
 using AvaloniaEdit.Document;
 using AvaloniaEdit.Rendering;
-using System.Globalization;
-using System.Text.RegularExpressions;
+using System;
 
 namespace Nikse.SubtitleEdit.Logic;
 
 public partial class SubtitleSyntaxHighlighting : DocumentColorizingTransformer
 {
-    private static readonly IBrush HtmlTagBrush = new SolidColorBrush(Color.Parse("#57A64A"));
-    private static readonly Typeface BoldTypeface = new(FontFamily.Default, weight: FontWeight.Bold);
-
-    [GeneratedRegex(@"</?(?:font[^>]*|[ibus]|sup|sub|ruby|rt)>", RegexOptions.IgnoreCase)]
-    private static partial Regex HtmlTagRegex();
-
-    [GeneratedRegex(@"<font[^>]*color\s*=\s*[""']?([^""'>\s]+)[""']?[^>]*>", RegexOptions.IgnoreCase)]
-    private static partial Regex FontColorRegex();
+    // Pastel color scheme for HTML syntax highlighting
+    private static readonly Color ElementColor = Color.FromRgb(183, 89, 155);    // Soft purple - HTML element tags (e.g., <div>, <span>)
+    private static readonly Color AttributeColor = Color.FromRgb(86, 156, 214);  // Soft blue - HTML attribute names (e.g., class, id, style)
+    private static readonly Color CommentColor = Color.FromRgb(106, 153, 85);    // Soft green - HTML comments (<!-- -->)
+    private static readonly Color CharsColor = Color.FromRgb(128, 128, 128);     // Gray - delimiters and special chars (<, >, ", ', =)
+    private static readonly Color ValuesColor = Color.FromRgb(206, 145, 120);    // Soft orange/peach - attribute values (e.g., "myclass")
+    private static readonly Color StyleColor = Color.FromRgb(156, 220, 254);     // Light cyan - CSS property values in style attribute
 
     protected override void ColorizeLine(DocumentLine line)
     {
-        var lineText = CurrentContext.Document.GetText(line);
-        if (string.IsNullOrEmpty(lineText))
+        var lineStartOffset = line.Offset;
+        var text = CurrentContext.Document.GetText(line);
+
+        var inComment = false;
+        var inHtmlTag = false;
+        var inAttributeVal = false;
+        var quoteChar = '\0';
+
+        for (int i = 0; i < text.Length; i++)
         {
-            return;
-        }
+            var c = text[i];
+            var c2 = i + 1 < text.Length ? text[i + 1] : '\0';
 
-        // Colorize HTML tags
-        ColorizeHtmlTags(line, lineText);
-
-        // Apply actual colors from font color attributes
-        ApplyFontColors(line, lineText);
-    }
-
-    private void ColorizeHtmlTags(DocumentLine line, string lineText)
-    {
-        foreach (Match match in HtmlTagRegex().Matches(lineText))
-        {
-            ChangeLinePart(
-                line.Offset + match.Index,
-                line.Offset + match.Index + match.Length,
-                element =>
-                {
-                    element.TextRunProperties.SetForegroundBrush(HtmlTagBrush);
-                    element.TextRunProperties.SetTypeface(BoldTypeface);
-                });
-        }
-    }
-
-    private void ApplyFontColors(DocumentLine line, string lineText)
-    {
-        var fontColorMatches = FontColorRegex().Matches(lineText);
-        if (fontColorMatches.Count == 0)
-        {
-            return;
-        }
-
-        var colorStack = new System.Collections.Generic.Stack<(int endPos, IBrush brush)>();
-
-        // Build a list of all tags (opening font with color, closing font)
-        var tags = new System.Collections.Generic.List<(int pos, bool isOpening, string? color)>();
-
-        foreach (Match match in fontColorMatches)
-        {
-            var colorValue = match.Groups[1].Value;
-            tags.Add((match.Index + match.Length, true, colorValue));
-        }
-
-        // Find all closing font tags
-        foreach (Match match in Regex.Matches(lineText, @"</font>", RegexOptions.IgnoreCase))
-        {
-            tags.Add((match.Index, false, null));
-        }
-
-        // Sort tags by position
-        tags.Sort((a, b) => a.pos.CompareTo(b.pos));
-
-        foreach (var tag in tags)
-        {
-            if (tag.isOpening && tag.color != null)
+            if (!inComment && c == '<')
             {
-                // Find the matching closing tag
-                var closingPos = FindMatchingClosingTag(lineText, tag.pos);
-                if (closingPos > tag.pos)
+                if (i + 3 < text.Length && c2 == '!' && text[i + 2] == '-' && text[i + 3] == '-')
                 {
-                    var brush = ParseColorToBrush(tag.color);
-                    if (brush != null)
+                    // Comment start: <!--
+                    var commentEnd = text.IndexOf("-->", i + 4, StringComparison.Ordinal);
+                    var commentLength = commentEnd != -1 ? commentEnd + 3 - i : text.Length - i;
+                    ChangeLinePart(lineStartOffset + i, lineStartOffset + i + commentLength, element =>
                     {
-                        ChangeLinePart(
-                            line.Offset + tag.pos,
-                            line.Offset + closingPos,
-                            element => element.TextRunProperties.SetForegroundBrush(brush));
+                        element.TextRunProperties.SetForegroundBrush(new SolidColorBrush(CommentColor));
+                    });
+                    i += commentLength - 1;
+                    continue;
+                }
+                else
+                {
+                    // HTML tag start
+                    ChangeLinePart(lineStartOffset + i, lineStartOffset + i + 1, element =>
+                    {
+                        element.TextRunProperties.SetForegroundBrush(new SolidColorBrush(CharsColor));
+                    });
+
+                    if (c2 == '/')
+                    {
+                        // Closing tag
+                        ChangeLinePart(lineStartOffset + i + 1, lineStartOffset + i + 2, element =>
+                        {
+                            element.TextRunProperties.SetForegroundBrush(new SolidColorBrush(CharsColor));
+                        });
+                        i++;
+                    }
+
+                    inHtmlTag = true;
+
+                    // Find element name end
+                    var elementStart = i + 1;
+                    if (text[elementStart] == '/')
+                        elementStart++;
+
+                    var elementEnd = elementStart;
+                    while (elementEnd < text.Length && !char.IsWhiteSpace(text[elementEnd]) &&
+                           text[elementEnd] != '>' && text[elementEnd] != '/')
+                    {
+                        elementEnd++;
+                    }
+
+                    if (elementEnd > elementStart)
+                    {
+                        ChangeLinePart(lineStartOffset + elementStart, lineStartOffset + elementEnd, element =>
+                        {
+                            element.TextRunProperties.SetForegroundBrush(new SolidColorBrush(ElementColor));
+                        });
+                        i = elementEnd - 1;
                     }
                 }
             }
+            else if (inHtmlTag && c == '>')
+            {
+                // HTML tag end
+                ChangeLinePart(lineStartOffset + i, lineStartOffset + i + 1, element =>
+                {
+                    element.TextRunProperties.SetForegroundBrush(new SolidColorBrush(CharsColor));
+                });
+                inHtmlTag = false;
+                inAttributeVal = false;
+                quoteChar = '\0';
+            }
+            else if (inHtmlTag && c == '/' && c2 == '>')
+            {
+                // Self-closing tag
+                ChangeLinePart(lineStartOffset + i, lineStartOffset + i + 2, element =>
+                {
+                    element.TextRunProperties.SetForegroundBrush(new SolidColorBrush(CharsColor));
+                });
+                inHtmlTag = false;
+                inAttributeVal = false;
+                quoteChar = '\0';
+                i++;
+            }
+            else if (inHtmlTag && !inAttributeVal && char.IsLetter(c))
+            {
+                // Attribute name
+                var attrStart = i;
+                while (i < text.Length && (char.IsLetterOrDigit(text[i]) || text[i] == '-' || text[i] == '_'))
+                {
+                    i++;
+                }
+
+                ChangeLinePart(lineStartOffset + attrStart, lineStartOffset + i, element =>
+                {
+                    element.TextRunProperties.SetForegroundBrush(new SolidColorBrush(AttributeColor));
+                });
+                i--;
+            }
+            else if (inHtmlTag && c == '=')
+            {
+                // Equals sign
+                ChangeLinePart(lineStartOffset + i, lineStartOffset + i + 1, element =>
+                {
+                    element.TextRunProperties.SetForegroundBrush(new SolidColorBrush(CharsColor));
+                });
+            }
+            else if (inHtmlTag && (c == '"' || c == '\''))
+            {
+                if (!inAttributeVal)
+                {
+                    // Start of attribute value
+                    quoteChar = c;
+                    inAttributeVal = true;
+                    var valueStart = i;
+                    var valueEnd = text.IndexOf(quoteChar, i + 1);
+                    if (valueEnd == -1)
+                        valueEnd = text.Length;
+                    else
+                        valueEnd++;
+
+                    // Color the quotes
+                    ChangeLinePart(lineStartOffset + valueStart, lineStartOffset + valueStart + 1, element =>
+                    {
+                        element.TextRunProperties.SetForegroundBrush(new SolidColorBrush(CharsColor));
+                    });
+
+                    // Color the value content (check for style attribute)
+                    var hasColon = text.IndexOf(':', i + 1, valueEnd - i - 2) != -1;
+                    var valueColor = hasColon ? StyleColor : ValuesColor;
+
+                    if (valueEnd > valueStart + 1)
+                    {
+                        ChangeLinePart(lineStartOffset + valueStart + 1, lineStartOffset + valueEnd - 1, element =>
+                        {
+                            element.TextRunProperties.SetForegroundBrush(new SolidColorBrush(valueColor));
+                        });
+
+                        // Color closing quote
+                        ChangeLinePart(lineStartOffset + valueEnd - 1, lineStartOffset + valueEnd, element =>
+                        {
+                            element.TextRunProperties.SetForegroundBrush(new SolidColorBrush(CharsColor));
+                        });
+                    }
+
+                    i = valueEnd - 1;
+                    inAttributeVal = false;
+                    quoteChar = '\0';
+                }
+            }
         }
-    }
-
-    private static int FindMatchingClosingTag(string text, int startPos)
-    {
-        var closingTagMatch = Regex.Match(text[startPos..], @"</font>", RegexOptions.IgnoreCase);
-        return closingTagMatch.Success ? startPos + closingTagMatch.Index : text.Length;
-    }
-
-    private static IBrush? ParseColorToBrush(string colorValue)
-    {
-        try
-        {
-            // Handle #RRGGBB format
-            if (colorValue.StartsWith('#'))
-            {
-                return new SolidColorBrush(Color.Parse(colorValue));
-            }
-
-            // Handle named colors (e.g., "red", "blue")
-            if (Color.TryParse(colorValue, out var namedColor))
-            {
-                return new SolidColorBrush(namedColor);
-            }
-
-            // Handle rgb(r,g,b) format
-            var rgbMatch = Regex.Match(colorValue, @"rgb\(\s*(\d+)\s*,\s*(\d+)\s*,\s*(\d+)\s*\)", RegexOptions.IgnoreCase);
-            if (rgbMatch.Success)
-            {
-                var r = byte.Parse(rgbMatch.Groups[1].Value);
-                var g = byte.Parse(rgbMatch.Groups[2].Value);
-                var b = byte.Parse(rgbMatch.Groups[3].Value);
-                return new SolidColorBrush(Color.FromRgb(r, g, b));
-            }
-
-            // Handle legacy hex format without #
-            if (colorValue.Length == 6 && int.TryParse(colorValue, NumberStyles.HexNumber, null, out _))
-            {
-                return new SolidColorBrush(Color.Parse("#" + colorValue));
-            }
-        }
-        catch
-        {
-            // Invalid color format, ignore
-        }
-
-        return null;
     }
 }
