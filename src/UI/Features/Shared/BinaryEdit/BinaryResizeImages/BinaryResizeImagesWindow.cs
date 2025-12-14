f429949c7a0b91717096afdd580f9b4bf9aaf63b
Nikolaj Olsson <nikse.dk@gmail.com>
2025-12-14 16:59:16 +0100

    Work on binedit

--- src/UI/Features/Shared/BinaryEdit/BinaryResizeImages/BinaryResizeImagesWindow.cs
+++ src/UI/Features/Shared/BinaryEdit/BinaryResizeImages/BinaryResizeImagesWindow.cs
@@ -71,39 +71,14 @@ public class BinaryResizeImagesWindow : Window
             Spacing = 10,
         };
 
-        // Resize mode
-        var resizeModeLabel = new TextBlock
-        {
-            Text = "Resize mode:",
-            FontWeight = Avalonia.Media.FontWeight.Bold,
-            Margin = new Thickness(0, 0, 0, 5),
-        };
-        panel.Children.Add(resizeModeLabel);
-
-        var resizeModeCombo = new ComboBox
-        {
-            ItemsSource = vm.ResizeModes,
-            Width = double.NaN,
-            HorizontalAlignment = HorizontalAlignment.Stretch,
-            [!ComboBox.SelectedItemProperty] = new Binding(nameof(vm.SelectedResizeMode)) { Mode = BindingMode.TwoWay },
-        };
-        panel.Children.Add(resizeModeCombo);
-
         // Percentage
-        var percentagePanel = new StackPanel
-        {
-            Orientation = Orientation.Vertical,
-            Margin = new Thickness(0, 10, 0, 0),
-            [!StackPanel.IsVisibleProperty] = new Binding(nameof(vm.IsPercentageVisible)),
-        };
-
         var percentageLabel = new TextBlock
         {
             Text = "Percentage:",
             FontWeight = Avalonia.Media.FontWeight.Bold,
             Margin = new Thickness(0, 0, 0, 5),
         };
-        percentagePanel.Children.Add(percentageLabel);
+        panel.Children.Add(percentageLabel);
 
         var percentageUpDown = new NumericUpDown
         {
@@ -118,96 +93,30 @@ public class BinaryResizeImagesWindow : Window
                 Converter = new NullableIntConverter(),
             },
         };
-        percentagePanel.Children.Add(percentageUpDown);
-        panel.Children.Add(percentagePanel);
+        panel.Children.Add(percentageUpDown);
 
-        // Fixed size
-        var fixedSizePanel = new StackPanel
+        // Image size display
+        var imageSizeLabel = new TextBlock
         {
-            Orientation = Orientation.Vertical,
-            Margin = new Thickness(0, 10, 0, 0),
-            [!StackPanel.IsVisibleProperty] = new Binding(nameof(vm.IsFixedSizeVisible)),
+            [!TextBlock.TextProperty] = new Binding(nameof(vm.ImageSizeText)),
+            TextWrapping = Avalonia.Media.TextWrapping.Wrap,
+            Margin = new Thickness(0, 20, 0, 0),
+            FontSize = 12,
+            FontWeight = Avalonia.Media.FontWeight.SemiBold,
         };
+        panel.Children.Add(imageSizeLabel);
 
-        var widthLabel = new TextBlock
+        // Info text
+        var infoText = new TextBlock
         {
-            Text = "Width:",
-            FontWeight = Avalonia.Media.FontWeight.Bold,
-            Margin = new Thickness(0, 0, 0, 5),
+            Text = "Enter the percentage to resize images.\n" +
+                   "Preview updates automatically.",
+            TextWrapping = Avalonia.Media.TextWrapping.Wrap,
+            Margin = new Thickness(0, 20, 0, 0),
+            FontSize = 11,
+            Foreground = Avalonia.Media.Brushes.Gray,
         };
-        fixedSizePanel.Children.Add(widthLabel);
-
-        var widthUpDown = new NumericUpDown
-        {
-            Minimum = 1,
-            Maximum = 10000,
-            Increment = 10,
-            FormatString = "0",
-            Width = double.NaN,
-            [!NumericUpDown.ValueProperty] = new Binding(nameof(vm.FixedWidth))
-            {
-                Mode = BindingMode.TwoWay,
-                Converter = new NullableIntConverter(),
-            },
-        };
-        fixedSizePanel.Children.Add(widthUpDown);
-
-        var heightLabel = new TextBlock
-        {
-            Text = "Height:",
-            FontWeight = Avalonia.Media.FontWeight.Bold,
-            Margin = new Thickness(0, 10, 0, 5),
-        };
-        fixedSizePanel.Children.Add(heightLabel);
-
-        var heightUpDown = new NumericUpDown
-        {
-            Minimum = 1,
-            Maximum = 10000,
-            Increment = 10,
-            FormatString = "0",
-            Width = double.NaN,
-            [!NumericUpDown.ValueProperty] = new Binding(nameof(vm.FixedHeight))
-            {
-                Mode = BindingMode.TwoWay,
-                Converter = new NullableIntConverter(),
-            },
-        };
-        fixedSizePanel.Children.Add(heightUpDown);
-
-        var maintainAspectCheckBox = new CheckBox
-        {
-            Content = "Maintain aspect ratio",
-            Margin = new Thickness(0, 10, 0, 0),
-            [!CheckBox.IsCheckedProperty] = new Binding(nameof(vm.MaintainAspectRatio)) { Mode = BindingMode.TwoWay },
-        };
-        fixedSizePanel.Children.Add(maintainAspectCheckBox);
-
-        panel.Children.Add(fixedSizePanel);
-
-        // Filter quality
-        var filterLabel = new TextBlock
-        {
-            Text = "Filter quality:",
-            FontWeight = Avalonia.Media.FontWeight.Bold,
-            Margin = new Thickness(0, 10, 0, 5),
-        };
-        panel.Children.Add(filterLabel);
-
-        var filterCombo = new ComboBox
-        {
-            ItemsSource = vm.FilterQualities,
-            Width = double.NaN,
-            HorizontalAlignment = HorizontalAlignment.Stretch,
-            [!ComboBox.SelectedItemProperty] = new Binding(nameof(vm.SelectedFilterQuality)) { Mode = BindingMode.TwoWay },
-        };
-        panel.Children.Add(filterCombo);
-
-        // Preview button
-        var previewButton = UiUtil.MakeButton(vm.UpdatePreviewCommand, IconNames.Refresh);
-        previewButton.Margin = new Thickness(0, 20, 0, 0);
-        previewButton.HorizontalAlignment = HorizontalAlignment.Stretch;
-        panel.Children.Add(previewButton);
+        panel.Children.Add(infoText);
 
         return panel;
     }
