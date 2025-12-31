bf22fc83bb3215cfe8da1bec4324baf694139ee9
Nikolaj Olsson <nikse.dk@gmail.com>
2025-12-31 06:55:41 +0100

    refact

--- src/UI/Features/Ocr/Engines/OcrEngineItem.cs
+++ src/UI/Features/Ocr/Engines/OcrEngineItem.cs
@@ -34,7 +34,7 @@ public class OcrEngineItem
         list.Add(new("Binary image compare", OcrEngineType.BinaryImageCompare, "OCR via image compare (free/open source)", "", ""));
         list.Add(new("Tesseract", OcrEngineType.Tesseract, "Tesseract is an open-source OCR engine", "", ""));
 
-        if (!OperatingSystem.IsMacOS())
+        if (OperatingSystem.IsWindows() || OperatingSystem.IsLinux())
         {
             list.Add(new("Paddle OCR Standalone", OcrEngineType.PaddleOcrStandalone, "Paddle OCR Standalone", "", ""));
         }
@@ -42,7 +42,7 @@ public class OcrEngineItem
         list.Add(new("Paddle OCR Python", OcrEngineType.PaddleOcrPython, "Paddle OCR Python", "", ""));
         list.Add(new("Ollama", OcrEngineType.Ollama, "Ollama e.g. via llama-vision", "", "http://localhost:11434/api/chat"));
         list.Add(new("Google Vision", OcrEngineType.GoogleVision, "Google Vision is a cloud-based OCR engine by Google", "", ""));
-        
+
         //list.Add(new("Azure Vision", OcrEngineType.AzureVision, "Azure Vision is a cloud-based OCR engine by Microsoft", "", ""));
         //list.Add(new("Amazon Rekognition", OcrEngineType.AmazonRekognition, "Amazon Rekognition is a cloud-based OCR engine by Amazon", "", ""));
 
