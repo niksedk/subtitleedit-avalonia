949236a5804fc5bf12c60614e94d0ed65910f0a6
Nikolaj Olsson <nikse.dk@gmail.com>
2026-01-25 13:57:53 +0100

    Try to fi text box sync - thx abc16361 :) Try to fix #289

--- src/UI/Features/Main/Layout/TextEditorBindingCoordinator.cs
+++ src/UI/Features/Main/Layout/TextEditorBindingCoordinator.cs
@@ -7,6 +7,7 @@ public class TextEditorBindingCoordinator
     private readonly MainViewModel _vm;
     private readonly TextEditorBindingHelper? _textEditorHelper;
     private readonly TextEditorBindingHelper? _originalTextEditorHelper;
+    private SubtitleLineViewModel? _currentlySubscribedSubtitle;
 
     public TextEditorBindingCoordinator(
         MainViewModel vm,
@@ -21,19 +22,54 @@ public class TextEditorBindingCoordinator
     public void Initialize()
     {
         _vm.PropertyChanged += OnViewModelPropertyChanged;
+        SubscribeToSelectedSubtitle();
     }
 
     public void DeInitialize()
     {
         _vm.PropertyChanged -= OnViewModelPropertyChanged;
+        UnsubscribeFromSelectedSubtitle();
     }
 
     private void OnViewModelPropertyChanged(object? sender, PropertyChangedEventArgs e)
     {
         if (e.PropertyName == nameof(_vm.SelectedSubtitle))
         {
+            UnsubscribeFromSelectedSubtitle();
+            SubscribeToSelectedSubtitle();
+            
             _textEditorHelper?.OnSelectedSubtitleChanged();
             _originalTextEditorHelper?.OnSelectedSubtitleChanged();
+        }
+    }
+
+    private void SubscribeToSelectedSubtitle()
+    {
+        if (_vm.SelectedSubtitle != null)
+        {
+            _currentlySubscribedSubtitle = _vm.SelectedSubtitle;
+            _currentlySubscribedSubtitle.PropertyChanged += OnSelectedSubtitlePropertyChanged;
+        }
+    }
+
+    private void UnsubscribeFromSelectedSubtitle()
+    {
+        if (_currentlySubscribedSubtitle != null)
+        {
+            _currentlySubscribedSubtitle.PropertyChanged -= OnSelectedSubtitlePropertyChanged;
+            _currentlySubscribedSubtitle = null;
+        }
+    }
+
+    private void OnSelectedSubtitlePropertyChanged(object? sender, PropertyChangedEventArgs e)
+    {
+        if (e.PropertyName == nameof(SubtitleLineViewModel.Text))
+        {
+            _textEditorHelper?.OnSubtitleTextPropertyChanged();
+        }
+        else if (e.PropertyName == nameof(SubtitleLineViewModel.OriginalText))
+        {
+            _originalTextEditorHelper?.OnSubtitleTextPropertyChanged();
         }
     }
 }
