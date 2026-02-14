4f08fe1514f5c20a6e29510324a5c51e52712f02
Nikolaj Olsson <nikse.dk@gmail.com>
2026-02-14 13:15:05 +0100

    A little more image optimize

--- src/UI/Logic/SkBitmapExtensions.cs
+++ src/UI/Logic/SkBitmapExtensions.cs
@@ -9,88 +9,6 @@ namespace Nikse.SubtitleEdit.Logic;
 
 internal static class SkBitmapExtensions
 {
-    public static SKBitmap ConvertToGrayscale(SKBitmap originalBitmap)
-    {
-        var grayscaleBitmap = new SKBitmap(originalBitmap.Width, originalBitmap.Height);
-        using var canvas = new SKCanvas(grayscaleBitmap);
-        using var paint = new SKPaint();
-        paint.ColorFilter = SKColorFilter.CreateColorMatrix(new[]
-        {
-            0.21f, 0.72f, 0.07f, 0, 0,
-            0.21f, 0.72f, 0.07f, 0, 0,
-            0.21f, 0.72f, 0.07f, 0, 0,
-            0,     0,     0,     1, 0
-        });
-        canvas.DrawBitmap(originalBitmap, 0, 0, paint);
-
-        return grayscaleBitmap;
-    }
-
-    public static SKBitmap ConvertToGrayscale(byte[] originalBitmap)
-    {
-        using var ms = new MemoryStream(originalBitmap);
-        var bitmap = SKBitmap.Decode(ms);
-        return ConvertToGrayscale(bitmap);
-    }
-
-    public static SKBitmap AdjustColors(SKBitmap bitmap, float redIncrease, float greenIncrease, float blueIncrease)
-    {
-        var grayscaleBitmap = new SKBitmap(bitmap.Width, bitmap.Height);
-        using var canvas = new SKCanvas(grayscaleBitmap);
-        using var paint = new SKPaint();
-        paint.ColorFilter = SKColorFilter.CreateColorMatrix(new[]
-        {
-            1.0f + redIncrease, 1.0f, 1.0f, 0, 0,
-            1.0f, 1.0f + greenIncrease, 1.0f, 0, 0,
-            1.0f, 1.0f, 1.0f + blueIncrease, 0, 0,
-            0,     0,     0,     1, 0
-        });
-        canvas.DrawBitmap(bitmap, 0, 0, paint);
-
-        return grayscaleBitmap;
-    }
-
-    public static SKBitmap AdjustColors(byte[] originalBitmap, float redIncrease, float greenIncrease, float blueIncrease)
-    {
-        using var ms = new MemoryStream(originalBitmap);
-        var bitmap = SKBitmap.Decode(ms);
-        return AdjustColors(bitmap, redIncrease, greenIncrease, blueIncrease);
-    }
-
-    public static MemoryStream BitmapToPngStream(SKBitmap bitmap)
-    {
-        var ms = new MemoryStream();
-        using var image = SKImage.FromBitmap(bitmap);
-        using var data = image.Encode(SKEncodedImageFormat.Png, 100);
-        data.SaveTo(ms);
-
-        return ms;
-    }
-
-    public static SKBitmap AddBorder(SKBitmap originalBitmap, int borderWidth, SKColor color)
-    {
-        var borderedBitmap = new SKBitmap(originalBitmap.Width, originalBitmap.Height);
-        using var canvas = new SKCanvas(borderedBitmap);
-        canvas.DrawBitmap(originalBitmap, borderWidth, borderWidth);
-
-        using var borderPaint = new SKPaint
-        {
-            Color = color,
-            Style = SKPaintStyle.Stroke,
-            StrokeWidth = borderWidth
-        };
-        canvas.DrawRect(0, 0, originalBitmap.Width, originalBitmap.Height, borderPaint);
-
-        return borderedBitmap;
-    }
-
-    public static SKBitmap AddBorder(byte[] originalBitmap, int borderWidth, SKColor color)
-    {
-        using var ms = new MemoryStream(originalBitmap);
-        var bitmap = SKBitmap.Decode(ms);
-        return AddBorder(bitmap, borderWidth, color);
-    }
-
     public static SKBitmap MakeImageBrighter(SKBitmap bitmap, float brightnessIncrease = 0.25f)
     {
         using var canvas = new SKCanvas(bitmap);
@@ -108,13 +26,6 @@ internal static class SkBitmapExtensions
         canvas.DrawBitmap(bitmap, 0, 0, paint);
 
         return bitmap;
-    }
-
-    public static SKBitmap MakeImageBrighter(byte[] originalBitmap, float brightnessIncrease = 0.25f)
-    {
-        using var ms = new MemoryStream(originalBitmap);
-        var bitmap = SKBitmap.Decode(ms);
-        return MakeImageBrighter(bitmap, brightnessIncrease);
     }
 
     public static unsafe SKBitmap CropTransparentColors(this SKBitmap originalBitmap, byte alphaThreshold = 0)
@@ -230,25 +141,27 @@ internal static class SkBitmapExtensions
         return data.ToArray();
     }
 
-    public static Bitmap ToAvaloniaBitmapOld(this SKBitmap skBitmap)
-    {
-        if (skBitmap.Width == 0 || skBitmap.Height == 0)
-        {
-            skBitmap = new SKBitmap(1, 1, true);
-        }
+    // Original simple code for ToAvaloniaBitmap :
+    //public static Bitmap ToAvaloniaBitmapOld(this SKBitmap skBitmap)
+    //{
+    //    if (skBitmap.Width == 0 || skBitmap.Height == 0)
+    //    {
+    //        skBitmap = new SKBitmap(1, 1, true);
+    //    }
 
-        using var image = SKImage.FromBitmap(skBitmap);
-        using var data = image.Encode(SKEncodedImageFormat.Png, 100);
-        using var stream = new MemoryStream(data.ToArray());
+    //    using var image = SKImage.FromBitmap(skBitmap);
+    //    using var data = image.Encode(SKEncodedImageFormat.Png, 100);
+    //    using var stream = new MemoryStream(data.ToArray());
 
-        return new Bitmap(stream);
-    }
-
+    //    return new Bitmap(stream);
+    //}
 
     public static Bitmap ToAvaloniaBitmap(this SKBitmap skBitmap)
     {
         if (skBitmap.Width <= 0 || skBitmap.Height <= 0)
+        {
             return new WriteableBitmap(new PixelSize(1, 1), Vector.One, PixelFormat.Bgra8888, AlphaFormat.Premul);
+        }
 
         var bitmap = new WriteableBitmap(
             new PixelSize(skBitmap.Width, skBitmap.Height),
@@ -268,13 +181,13 @@ internal static class SkBitmapExtensions
                 byte* srcBase = (byte*)skBitmap.GetPixels();
                 byte* dstBase = (byte*)lockedBitmap.Address;
 
-                for (int y = 0; y < height; y++)
+                for (var y = 0; y < height; y++)
                 {
                     // Pointer arithmetic is faster than repeated multiplication
                     uint* srcRow = (uint*)(srcBase + (y * srcStride));
                     uint* dstRow = (uint*)(dstBase + (y * dstStride));
 
-                    for (int x = 0; x < width; x++)
+                    for (var x = 0; x < width; x++)
                     {
                         uint pixel = srcRow[x];
                         uint a = pixel >> 24;
@@ -294,10 +207,10 @@ internal static class SkBitmapExtensions
                             uint r = (pixel >> 16) & 0xFF;
                             uint g = (pixel >> 8) & 0xFF;
                             uint b = pixel & 0xFF;
-
-                            r = (r * a + 128) >> 8;
-                            g = (g * a + 128) >> 8;
-                            b = (b * a + 128) >> 8;
+                            var a128 = a + 128; // Precompute alpha + 128 for reuse
+                            r = (r * a128) >> 8;
+                            g = (g * a128) >> 8;
+                            b = (b * a128) >> 8;
 
                             dstRow[x] = (a << 24) | (r << 16) | (g << 8) | b;
                         }
@@ -309,14 +222,14 @@ internal static class SkBitmapExtensions
         return bitmap;
     }
 
-    public static SKBitmap ToSkBitmapOld(this Bitmap avaloniaBitmap)
-    {
-        using var stream = new MemoryStream();
-        avaloniaBitmap.Save(stream);
-        stream.Seek(0, SeekOrigin.Begin);
-        return SKBitmap.Decode(stream);
-    }
-
+    // Original simple code for ToSkBitmap:
+    //public static SKBitmap ToSkBitmapOld(this Bitmap avaloniaBitmap)
+    //{
+    //    using var stream = new MemoryStream();
+    //    avaloniaBitmap.Save(stream);
+    //    stream.Seek(0, SeekOrigin.Begin);
+    //    return SKBitmap.Decode(stream);
+    //}
 
     public static SKBitmap ToSkBitmap(this Bitmap avaloniaBitmap)
     {
